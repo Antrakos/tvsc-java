fb6c78fe1e0728f7ae5e612e5173b153905b48eb
Taras Zubrei <taraszubrei@gmail.com>
2016-07-24 18:34:37 +0300

    Jmh spring integration

--- service/src/jmh/groovy/com/tvsc/service/performance/JsonUtilsPerformanceTest.groovy
+++ service/src/jmh/groovy/com/tvsc/service/performance/JsonUtilsPerformanceTest.groovy
@@ -3,10 +3,9 @@ package com.tvsc.service.performance
 import com.tvsc.core.model.Episode
 import com.tvsc.service.config.ServiceConfig
 import com.tvsc.service.util.JsonUtils
-import com.tvsc.service.util.impl.GsonUtilsImpl
-import com.tvsc.service.util.impl.JsonUtilsImpl
 import org.openjdk.jmh.annotations.*
 import org.openjdk.jmh.infra.Blackhole
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import java.util.concurrent.TimeUnit
 
@@ -23,16 +22,11 @@ class JsonUtilsPerformanceTest {
     final static String template = """{"id": 4894849,"airedSeason": 2,"episodeName": "The Race of His Life","firstAired": "2016-05-24","overview": "Barry vows to stop Zoom after learning Zoom's true plans.","lastUpdated": 1466154212,"filename": "episodes/279121/5598674.jpg","seriesId": 279121,"imdbId": "tt5215758","siteRating": 8}"""
     final static String jsonObject = """{"data": {"id": 5598674,"airedSeason": 2,"airedEpisodeNumber": 23,"episodeName": "The Race of His Life","firstAired": "2016-05-24","overview": "Barry vows to stop Zoom after learning Zoom's true plans.","lastUpdated": 1466154212,"filename": "episodes/279121/5598674.jpg","seriesId": 279121,"imdbId": "tt5215758","siteRating": 8}}"""
 
-    static String jsonList
+    final static String jsonList = """{"data": [""" + (1..100).collect { String.format(template) }.join(',') + """]}"""
 
-    final static ServiceConfig config = new ServiceConfig()
-    final static JsonUtils jsonUtils = new JsonUtilsImpl(config.objectMapper())
-    final static JsonUtils gsonUtils = new GsonUtilsImpl(config.gson())
-
-    @Setup
-    void setup() {
-        jsonList = """{"data": [""" + (1..100).collect { String.format(template) }.join(',') + """]}"""
-    }
+    final static def context = new AnnotationConfigApplicationContext(ServiceConfig)
+    final static JsonUtils jsonUtils = context.getBean('jsonUtilsImpl') as JsonUtils
+    final static JsonUtils gsonUtils = context.getBean('gsonUtilsImpl') as JsonUtils
 
     @Benchmark
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
--- service/src/jmh/groovy/com/tvsc/service/performance/PaginationUtilsPerformanceUnitTest.groovy
+++ service/src/jmh/groovy/com/tvsc/service/performance/PaginationUtilsPerformanceUnitTest.groovy
@@ -5,11 +5,10 @@ import com.tvsc.service.Constants
 import com.tvsc.service.config.ServiceConfig
 import com.tvsc.service.util.HttpUtils
 import com.tvsc.service.util.JsonUtils
-import com.tvsc.service.util.impl.JsonUtilsImpl
 import com.tvsc.service.util.impl.PaginationUtilsImpl
-import org.jetbrains.annotations.NotNull
 import org.openjdk.jmh.annotations.*
 import org.openjdk.jmh.infra.Blackhole
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.TimeUnit
@@ -24,30 +23,20 @@ import java.util.concurrent.TimeUnit
 @BenchmarkMode(Mode.All)
 @Fork(value = 1, jvmArgs = "-server", warmups = 0, jvmArgsAppend = "-XX:+UseCompressedOops")
 class PaginationUtilsPerformanceUnitTest {
-    private PaginationUtilsImpl paginationUtils
-    private HttpUtils httpUtils
-    private JsonUtils jsonUtils
-
     final static String template = """{"links": {"last": %d}, "data": [{"id": %d,"airedSeason": %d,"airedEpisodeNumber": 23,"episodeName": "The Race of His Life","firstAired": "2016-05-24","overview": "Barry vows to stop Zoom after learning Zoom's true plans.","lastUpdated": 1466154212,"filename": "episodes/279121/5598674.jpg","seriesId": 279121,"imdbId": "tt5215758","siteRating": 8}]}"""
     final static int pages = 5
     final static List<String> jsonPages = (1..pages).collect { String.format(template, pages, it * 100, it) }
 
-    @Setup
-    void setup() {
-        ServiceConfig config = new ServiceConfig()
-        jsonUtils = new JsonUtilsImpl(config.objectMapper())
-        httpUtils = new HttpUtils() {
-            @Override
-            CompletableFuture<String> get(@NotNull String url) {
-                CompletableFuture.supplyAsync {
-                    url.find(/page=\d+/) ?: 'page=1'
-                }.thenApply {
-                    PaginationUtilsPerformanceUnitTest.jsonPages[it[5..-1].toInteger() - 1]
-                }
-            }
+    final static def context = new AnnotationConfigApplicationContext(ServiceConfig)
+    final static JsonUtils jsonUtils = context.getBean(JsonUtils)
+    final static HttpUtils httpUtils = { String url ->
+        CompletableFuture.supplyAsync {
+            url.find(/page=\d+/) ?: 'page=1'
+        }.thenApply {
+            PaginationUtilsPerformanceUnitTest.jsonPages[it[5..-1].toInteger() - 1]
         }
-        paginationUtils = new PaginationUtilsImpl(jsonUtils, httpUtils)
     }
+    final static PaginationUtilsImpl paginationUtils = new PaginationUtilsImpl(jsonUtils, httpUtils)
 
     @Benchmark
     @OutputTimeUnit(TimeUnit.MICROSECONDS)
